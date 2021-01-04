953d2bbb019c0693a197a37c09acbfdc3eaba7bd
Nirzhor <rabbi.cse1.bu@gmail.com>
2021-01-04 23:14:50 +0600

    To-Do App v2 Firebase (removed )

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCjI-jq0zxc9GjNfuBC6dPfT4epcH8P7e4",
-    authDomain: "todoapp-v2-8701c.firebaseapp.com",
-    projectId: "todoapp-v2-8701c",
-    storageBucket: "todoapp-v2-8701c.appspot.com",
-    messagingSenderId: "688772549322",
-    appId: "1:688772549322:web:b468f8fa8910a2416f4233"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   firebase.initializeApp(firebaseConfig);
